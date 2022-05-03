8319f4027b708091a899900fa9444124ffcb762e
intisar11 <100384942+intisar11@users.noreply.github.com>
2022-05-03 10:18:40 -0700

    practice

--- Birthday Wisher (Day 32) start/main.py
+++ Birthday Wisher (Day 32) start/main.py
@@ -1,14 +1,29 @@
-import smtplib
-email = "intisar.a42@yahoo.com"
-password = "zarfgxzvnqkvhydl"
 
-with smtplib.SMTP("smtp.mail.yahoo.com") as connection:
-    connection.starttls()
-    connection.login(user=email, password=password)
-    connection.sendmail(from_addr= email,
-                        to_addrs="007764088@coyote.csusb.edu",
-                        msg="Subject:Hello\n\nThis is the body"
-)
+import datetime as DT
+
+import random
+import smtplib
+now = DT.datetime.today()
+weekday = now.weekday()
+email = "intisar.a42@yahoo.com"
+password = "jucfyusabmbptlei"
+print(weekday)
+if weekday == 1:
+
+    with open("quotes.txt", "r") as quotes:
+        quotes_list = quotes.read().split("\n")
+        choice = random.choice(quotes_list)
+        print(choice)
+
+    with smtplib.SMTP("smtp.mail.yahoo.com") as connection:
+        connection.starttls()
+        connection.login(user=email, password=password)
+        connection.sendmail(from_addr=email, to_addrs="007764088@coyote.csusb.edu",
+                            msg=f"Subject:Quotes\n\n{choice}")
+
+
+
+
 
 
 
