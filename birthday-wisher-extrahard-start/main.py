44ba3e5aa72aeb2ab7766f3172e74ee432ef43b0
intisar11 <100384942+intisar11@users.noreply.github.com>
2022-05-05 10:48:17 -0700

    practice

--- birthday-wisher-extrahard-start/main.py
+++ birthday-wisher-extrahard-start/main.py
@@ -1,19 +1,23 @@
 ##################### Extra Hard Starting Project ######################
 import datetime as dt
 import pandas as pd
+import smtplib
+import random
+email = "intisar.a42@yahoo.com"
+password = "mwyunjmofrkddjmh"
 # 1. Update the birthdays.csv
 
 # 2. Check if today matches a birthday in the birthdays.csv
 
 date_month = dt.datetime.today().month
 date_day = dt.datetime.today().day
-print(date_month)
-print(date_day)
+today = (date_month, date_day)
+print(today)
 
-birthday = pd.read_csv("birthdays.csv")
-birthday_dataframe = pd.DataFrame(birthday)
-month = birthday_dataframe['month']
-day = birthday_dataframe['day']
+data = pd.read_csv("birthdays.csv")
+
+new_dict = {(data_row['month'], data_row['day']): data_row for (index, data_row) in data.iterrows()}
+print(new_dict)
 
 
 
@@ -21,8 +25,16 @@ day = birthday_dataframe['day']
 
 # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
 
-# 4. Send the letter generated in step 3 to that person's email address.
+with open("./letter_templates/letter_1.txt") as letter1:
+    data = letter1.read()
+    data.replace("[NAME]", "Mom")
+    print(data)
 
+# 4. Send the letter generated in step 3 to that person's email address.
+with smtplib.SMTP as connection:
+    connection.starttls()
+    connection.login(email, password)
+    connection.sendmail()
 
 
 
